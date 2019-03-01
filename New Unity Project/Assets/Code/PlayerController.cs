fc5a05937a9ada2e2433b9c38b7ac80c4e10c96e
kristenYu <kristen.yu@du.edu>
2019-02-28 21:45:22 -0700

    LERP movement implemented"

--- New Unity Project/Assets/Code/PlayerController.cs
+++ New Unity Project/Assets/Code/PlayerController.cs
@@ -5,22 +5,58 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed = 5; 
+    public float speed = 5;
+
+    public Vector2 newPos;
+    public Vector2 currPos;
+    public Vector2 myPos;
+    public float fraction;
+    public float journeyLength;
+    public float startTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
-    {
+    { 
         if(Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            this.x -= speed;
+            MoveLeft();
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            MoveRight();
+        }
+
+        if (fraction < 1)
+        {
+            fraction = ((Time.time - startTime) * speed) / journeyLength;
+            this.transform.position = Vector2.Lerp(currPos, newPos, fraction);
+
         }
     }
 
-    
+
+
+    void MoveLeft()
+    {
+        currPos = this.transform.position;
+        newPos = new Vector2(this.transform.position.x - speed, this.transform.position.y);
+        journeyLength = Vector2.Distance(currPos, newPos);
+        startTime = Time.time;
+        fraction = 0;
+    }
+
+    void MoveRight()
+    {
+        currPos = transform.position;
+        newPos = new Vector2(this.transform.position.x + speed, this.transform.position.y);
+        journeyLength = Vector2.Distance(currPos, newPos);
+        startTime = Time.time;
+        fraction = 0;
+    }
+
 }
