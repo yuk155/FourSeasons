b58fcd4bb0b731c363f6b82d3d95323e265bcb90
kristenYu <kristen.yu@du.edu>
2019-03-01 18:29:28 -0700

    Working player controller

--- New Unity Project/Assets/Code/PlayerController.cs
+++ New Unity Project/Assets/Code/PlayerController.cs
@@ -2,9 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : PhysicsObject
 {
 
+    public float maxSpeed = 7;
+    public float jumpTakeOffSpeed = 7;
+
+    //private SpriteRenderer spriteRenderer;
+    //private Animator animator;
+
+    // Use this for initialization
+    void Awake()
+    {
+       // spriteRenderer = GetComponent<SpriteRenderer>();
+       // animator = GetComponent<Animator>();
+    }
+
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            velocity.y = jumpTakeOffSpeed;
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if (velocity.y > 0)
+            {
+                velocity.y = velocity.y * 0.5f;
+            }
+        }
+        /*
+        bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
+        if (flipSprite)
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+
+        animator.SetBool("grounded", grounded);
+        animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+        */
+        targetVelocity = move * maxSpeed;
+    }
+
+    /*
     public float speed = 5;
 
     public Vector2 newPos;
@@ -58,5 +102,7 @@ public class PlayerController : MonoBehaviour
         startTime = Time.time;
         fraction = 0;
     }
+
+    */
 
 }
