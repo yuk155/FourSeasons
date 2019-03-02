b5c4334bbc2dd3578b440337af3bf2598fcc88da
Aguiardavidm <Aguiardavidm@gmail.com>
2019-03-01 21:08:40 -0700

    Main Menu Fixes, Level Screen Build

--- New Unity Project/Assets/Code/MainMenuButton.cs
+++ New Unity Project/Assets/Code/MainMenuButton.cs
@@ -1,11 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuButton : MonoBehaviour
 {
     public void doExitGame()
     {
         Application.Quit();
+    }
+
+    public void goToLevelSelect()
+    {
+        SceneManager.LoadScene("LevelSelect");
+    }
+
+    public void goToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
