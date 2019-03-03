19cc4cff97e8ab1995d572e438d201f4a14d5278
Aguiardavidm <Aguiardavidm@gmail.com>
2019-03-03 02:00:45 -0700

    lots of small things

---
+++ New Unity Project/Assets/Code/LevelSelect.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Summer1()
+    {
+        SceneManager.LoadScene("Summer 1");
+    }
+
+    public void Fall1()
+    {
+        SceneManager.LoadScene("Fall 1");
+    }
+
+    public void Winter1()
+    {
+        SceneManager.LoadScene("Winter 1");
+    }
+
+    public void Spring1()
+    {
+        SceneManager.LoadScene("Spring 1");
+    }
+
+    public void Summer2()
+    {
+        SceneManager.LoadScene("Summer 2");
+    }
+
+    public void Fall2()
+    {
+        SceneManager.LoadScene("Fall 2");
+    }
+
+    public void Winter2()
+    {
+        SceneManager.LoadScene("Winter 2");
+    }
+
+    public void Spring2()
+    {
+        SceneManager.LoadScene("Spring 2");
+    }
+}
