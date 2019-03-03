4886a87889472e72bf83ae5b83f88f1825288f5b
kristenYu <kristen.yu@du.edu>
2019-03-02 18:43:52 -0700

    Water Implemented

---
+++ New Unity Project/Assets/Code/WaterBlock.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterBlock : MonoBehaviour
+{
+    public enum Direction { Up, Down, Right, Left, None }
+    public Direction direction; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        //direction = Direction.None;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
